37833db4fa1d197f9738c7ad17aee5781d68e128
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-21 19:35:36 +0300

    added a before hook on the mongoose.connect method to ensure every test runs only when we are connected to the database

--- test/create_test.js
+++ test/create_test.js
@@ -2,8 +2,11 @@ const assert = require("assert");
 const User = require("../src/models/users");
 
 describe("Creating records", () => {
-  it("Saves a user", () => {
+  it("Saves a user", done => {
     const brayo = new User({ name: "Brayo" });
-    brayo.save();
+    brayo.save().then(() => {
+      assert(!brayo.isNew);
+      done();
+    });
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/users_test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+before(done => {
+  mongoose.connect("mongodb://127.0.0.1/users_test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  mongoose.connection
+    .once("open", () => done())
+    .on("error", error => console.log("Error", error));
 });
-mongoose.connection
-  .once("open", () => console.log("Good to go"))
-  .on("error", error => console.log("Error", error));
 
 beforeEach(done => {
   mongoose.connection.collections.users.drop(() => {
