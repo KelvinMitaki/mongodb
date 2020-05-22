219ba104d4c432aae0b52a837a99c07376041590
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-22 09:14:43 +0300

    added a mocha test to assert that the users postcount is being incremented by one

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 before(done => {
   mongoose.connect("mongodb://127.0.0.1/users_test", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   });
   mongoose.connection
     .once("open", () => done())
---
+++ test/updating_test.js
@@ -0,0 +1,45 @@
+const assert = require("assert");
+const User = require("../src/models/users");
+
+describe("Updating users", () => {
+  let brayo;
+  beforeEach(async () => {
+    brayo = new User({ name: "Brayo", postCount: 0 });
+    await brayo.save();
+  });
+
+  const assertName = async operation => {
+    try {
+      await operation;
+      const users = await User.find({});
+      assert(users.length === 1);
+      assert(users[0].name === "Kevoh");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  it("instance type using set n save", async () => {
+    brayo.set("name", "Kevoh");
+    await assertName(brayo.save());
+  });
+  it("a model instance with update", async () => {
+    await assertName(brayo.updateOne({ name: "Kevoh" }));
+  });
+  it("a model class update of many ", async () => {
+    await assertName(User.updateMany({ name: "Brayo" }, { name: "Kevoh" }));
+  });
+  it("a model class of finding one and update", async () => {
+    await assertName(
+      User.findOneAndUpdate({ name: "Brayo" }, { name: "Kevoh" })
+    );
+  });
+  it("a model class of finding by ID and updating", async () => {
+    await assertName(User.findByIdAndUpdate(brayo._id, { name: "Kevoh" }));
+  });
+  it("Incrementing a users postCount by one", async () => {
+    await User.findOneAndUpdate({ name: "Brayo" }, { $inc: { postCount: 1 } });
+    const user = await User.findOne({ name: "Brayo" });
+    assert(user.postCount === 1);
+  });
+});
